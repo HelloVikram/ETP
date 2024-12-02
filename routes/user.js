a769b12294c701ebb382f26cf5b4c0316bf67ae3
HelloVikram <vikram.kumar.cs.2024@gmail.com>
2024-12-02 16:40:54 +0530

    Login backend

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,29 @@ const db = require('../Model/signup');
 const Sequelize=require('sequelize');
 const express = require('express');
 const router = express.Router();
-router.post('/user/signup', async (req, res, next) => {
+
+router.post('/user/login',async (req,res)=>{
+const email=req.body.email;
+const password=req.body.password;
+try{
+   const user=await db.findOne({where:{email}});
+   if(!user){
+      return res.status(404).json({success:false,message:'User not found'});
+   }
+   const ispassword=await db.findOne({where:{password}});
+   if(!ispassword){
+      return res.status(401).json({success:false,message:'User not authorized'});
+   }
+   return res.status(200).json({success:true,message:'User login Successfull'})
+
+
+}catch(err){
+  res.status(500).json({success:false,message:'Internal server error'})
+}
+})
+ 
+
+router.post('/user/signup', async (req, res) => {
    const name = req.body.name
    const email = req.body.email
    const password = req.body.password
