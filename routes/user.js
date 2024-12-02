a2fbfe889bd68fa269f387f1855d5d2c29e4a30c
HelloVikram <vikram.kumar.cs.2024@gmail.com>
2024-12-02 18:08:37 +0530

    using bcrypt

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const db = require('../Model/signup');
 const Sequelize=require('sequelize');
 const express = require('express');
 const router = express.Router();
-
+const bcrypt=require('bcrypt');
 router.post('/user/login',async (req,res)=>{
 const email=req.body.email;
 const password=req.body.password;
@@ -11,30 +11,38 @@ try{
    if(!user){
       return res.status(404).json({success:false,message:'User not found'});
    }
-   const ispassword=await db.findOne({where:{password}});
-   if(!ispassword){
-      return res.status(401).json({success:false,message:'User not authorized'});
+   bcrypt.compare(password,user.password,(err,result)=>{
+      if(err)
+         throw new Error('Something went wrong');
+   if(result==true){
+      return res.status(200).json({success:true,message:'User login Successfull'})
    }
-   return res.status(200).json({success:true,message:'User login Successfull'})
-
-
+   else{
+      return res.status(401).json({success:false,message:'Password is incorrect'})
+   }
+})
 }catch(err){
   res.status(500).json({success:false,message:'Internal server error'})
 }
 })
  
 
-router.post('/user/signup', async (req, res) => {
+router.post('/user/signup',  (req, res) => {
    const name = req.body.name
    const email = req.body.email
    const password = req.body.password
    try {
-      await db.create({
-         name: name,
-         email: email,
-         password: password
+      const saltrounds=10;
+      bcrypt.hash(password,saltrounds,async(err,hash)=>{
+         await db.create({
+            name: name,
+            email: email,
+            password: hash
+         })
+         res.status(200).json({ message: 'Database created successfully' })
       })
-      res.status(200).json({ signupdata: 'Database created successfully' })
+      
+      
    } catch (error) {
       if (error instanceof Sequelize.UniqueConstraintError) {
          res.status(409).json({ error: 'Email already exists' });
