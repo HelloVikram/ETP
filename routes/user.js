d421149aadf3832beaf848b7b6df5148c71b910f
HelloVikram <vikram.kumar.cs.2024@gmail.com>
2024-12-03 18:21:36 +0530

    Expense add and delete

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const db = require('../Model/signup');
+const expensedb=require('../Model/expense');
 const Sequelize=require('sequelize');
 const express = require('express');
 const router = express.Router();
 const bcrypt=require('bcrypt');
+
 router.post('/user/login',async (req,res)=>{
 const email=req.body.email;
 const password=req.body.password;
@@ -52,4 +54,42 @@ router.post('/user/signup',  (req, res) => {
       console.log("Error in creating database...");
    }
 })
+
+router.post('/expense/add-expense', async(req,res,next)=>{
+   const{amount,description,category}=req.body;
+   try{
+      const response= await expensedb.create({amount:amount,description:description,category:category});
+      console.log("Expense db created successfully");
+      res.status(201).json(response);
+   }catch(err){
+      res.status(500).json({Error:'Error in creating Expensedb'})
+   }
+   
+})
+
+router.get('/expense/get-expense',async(req,res)=>{
+   try{
+      const response=await expensedb.findAll();
+      res.status(200).json(response);
+   }catch(err){
+      res.status(400).json({Error:'Error in fetching data',details:err.message});
+   }
+})
+
+router.delete('/expense/delete-expense/:id',async(req,res)=>{
+       const eid=req.params.id;
+       if (isNaN(eid)) {
+         return res.status(400).json({ error: 'Invalid expense ID' });
+     }
+
+      try{
+       const response=await expensedb.destroy({where:{id:eid}}) ;
+       console.log(`Expense with ID ${eid} deleted successfully`);
+        res.status(200).json({ message: `Expense with ID ${eid} deleted successfully` });
+      }catch(err){
+         console.error('Error deleting expense:', err);
+         res.status(500).json({ error: 'Error deleting expense' });
+      }
+})
+
 module.exports = router;
